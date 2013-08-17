83e6cec3696f29c5f3a7becf64511c26244a5973
Hack Reactor Students <github@hackreactor.com>
2013-08-16 17:02:25 -0700

    create project files, add svg and enemies to page

---
+++ watchout.js
@@ -0,0 +1,36 @@
+var game = {};
+
+game.gameboard = d3.select(".stage").append("svg:svg").attr("width", 500).attr("height", 500);
+
+
+game.enemies = function(){
+  var enemyList = [];
+
+  for (i = 0; i < 20; i++) {
+    var enemy = {};
+    enemy.id = i;
+    enemy.x = Math.random() * 500;
+    enemy.y = Math.random() * 500;
+    enemyList.push(enemy);
+  }
+  return enemyList;
+}();
+
+game.gameboard.selectAll("circle.enemy").data(game.enemies).enter()
+  .append("svg:circle")
+  .attr("class", "enemy")
+  .attr("cx", function(d){
+    return d.x;
+  })
+  .attr("cy", function(d){
+    return d.y;
+  })
+  .attr("r", 20);
+
+console.log(game.enemies);
+
+
+//some function that works with d3 enter/"update"/exit
+
+//create the truth were going to pass to our d3 fn
+
